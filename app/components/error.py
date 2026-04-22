79d088ee089380d36a0d651be8de58c0715eb844
cosaquenodije <xavierfc.ti24@utsjr.edu.mx>
2026-04-22 06:12:39 -0600

    primer commit

---
+++ app/components/error.py
@@ -0,0 +1,9 @@
+class ApiError(Exception):
+    def __init__(self, message: str, status_code: int):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
+
+
+def api_error_to_text(e: ApiError) -> str:
+    return f"Error {e.status_code}: {e.message}"
---
+++ app/styles/estilos.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+class Colors:
+    BG = ft.Colors.BLUE_GREY_100
+
+class Textos_estilos:
+    H4 = ft.TextStyle(size=14, weight=ft.FontWeight.BOLD)
+
+class Card:
+    pass
